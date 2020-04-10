29fa9b42262a514c313bf3e13b7b41c357dbccdf
Dylan George Hsu <hsud3@mskcc.org>
2020-04-10 15:42:44 -0400

    Put the information needed for encoding Freesurfer label grouping, into hardcoded Python dicts

---
+++ testFsEncoding.py
@@ -0,0 +1,22 @@
+import SimpleITK as sitk
+from fsOneHotEncoding import fsDicts
+fsLabelDict, fsNamedGroups = fsDicts()
+# testing code
+aseg=sitk.ReadImage('/home/hsud3/AB21DF81F_aseg.nii.gz')
+ltlmf=sitk.LabelImageToLabelMapFilter()
+castImageFilter = sitk.CastImageFilter()
+castImageFilter.SetOutputPixelType(sitk.sitkUInt16)
+aseg_UInt16 = castImageFilter.Execute(aseg)
+labelShapeFilter = sitk.LabelShapeStatisticsImageFilter()
+labelShapeFilter.Execute(aseg_UInt16)
+presentLabels = labelShapeFilter.GetLabels()
+for mapping in fsLabelDict.items():
+  i=mapping[0]
+  name=mapping[1]
+  if i in presentLabels:
+    nvoxels = labelShapeFilter.GetNumberOfPixels(i)
+  else:
+    nvoxels = 0
+  print("%d\t%d\t%s"%(i,nvoxels,name))
+
+
